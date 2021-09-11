afe91a60edfaada76d149c1202833096786596e7
helenapaixao <hp.helenapaixao@gmail.com>
2021-09-11 20:31:10 -0300

    feat: config props active in button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from "react-modal";
-import incomeImg from '../../assets/income.svg';
-import outcomeImg from '../../assets/outcome.svg';
-import closeImg from '../../assets/close.svg'
-import { Container,TransactionTypeContainer } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import closeImg from "../../assets/close.svg";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import { useState } from "react";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -12,30 +13,44 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+
+  const [type, setType] = useState('deposit');
+
+ 
+  
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
       <button type="button">
-        <img src={closeImg} alt="Fechar Modal" onClick={onRequestClose}
-        className="react-modal-close"/>
+        <img
+          src={closeImg}
+          alt="Fechar Modal"
+          onClick={onRequestClose}
+          className="react-modal-close"
+        />
       </button>
       <Container>
         <h2>Cadastrar transações</h2>
         <input placeholder="Titulo" />
         <TransactionTypeContainer>
-          <button type="button">
+          <RadioBox
+            type="button"
+          onClick={() => setType('deposit')}
+          >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button type="button">
+          <RadioBox type="button"
+              onClick={() => setType('withdraw')}
+          >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
-
-
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input type="number" placeholder="Valor" />
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { darken } from "polished";
 export const Container = styled.div`
   h2 {
     color: var(--text-title);
@@ -45,6 +45,41 @@ export const Container = styled.div`
 `;
 
 export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
 
 
-`
+
+  img {
+    width: 28px;
+    height: 28px;
+  }
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+`;
+
+interface RadiosBoxProps {
+  isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadiosBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+
+    background: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+      border-color: ${darken(0.1, "#d7d7d7")};
+    }
+
+`;
