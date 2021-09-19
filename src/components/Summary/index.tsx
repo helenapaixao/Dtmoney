7a600530903a4a35935079f00dc4b26677782a59
helenapaixao <hp.helenapaixao@gmail.com>
2021-09-19 01:21:06 -0300

    calculate:ok

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,4 +1,4 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
@@ -7,33 +7,59 @@ import { TransactionsContext } from "../../TransactionContexts";
 import { Container } from "./styles";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
 
-  const {transactions} = useContext(TransactionsContext)
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
 
-  console.log(transactions)
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
 
   return (
     <Container>
-  
       <div>
         <header>
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
-          <strong>R$ 1000,00</strong>
+          <strong>
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(summary.deposits)}
+          </strong>
         </header>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
-          <strong>R$ 1000,00</strong>
+          <strong>
+            -
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(summary.withdraws)}
+          </strong>
         </header>
       </div>
       <div className="hightlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
-          <strong> - R$ 500,00</strong>
+          <strong>{summary.total}</strong>
         </header>
       </div>
     </Container>
